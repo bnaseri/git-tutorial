00f36077482f43231e00209754dd722ed84adf4e
Behzad <bnaseri.me@gmail.com>
2022-09-13 18:02:51 +0430

    added whatever

---
+++ app.js
@@ -0,0 +1,21 @@
+var x = 55;
+var y = isEven(x);
+logger(y);
+
+
+function logger(l){
+    console.log("Even function result is: ", l);
+}
+
+
+function isEven(value) {
+    if(value % 2 === 0){
+        return "Even";
+    }
+    else {
+        return "Odd";
+    }
+}
+function whatever{
+    
+}
